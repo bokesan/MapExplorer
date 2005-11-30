a9e5bf5de0716919105a84bfa4c8c9a61769fae6
Christoph Breitkopf <chbreitkopf@gmail.com>
2005-11-30 15:37:20 +0100

    Added GARGANTUAN and COLOSSAL. Added GARGANTUAN and COLOSSAL. Fixed indentation.

--- src/de/bokeh/ddm/mapexplorer/CreatureSize.java
+++ src/de/bokeh/ddm/mapexplorer/CreatureSize.java
@@ -5,19 +5,21 @@ package de.bokeh.ddm.mapexplorer;
  * @author Christoph Breitkopf
  */
 public enum CreatureSize {
-
-	TINY, SMALL, MEDIUM, LARGE, HUGE;
-
-	/**
-	 * Size in number of map squares.
-	 * @return The size in map squares for this size category.
-	 */
-	public int sizeSquares() {
-		switch (this) {
-		case LARGE: return 2;
-		case HUGE: return 3;
-		default: return 1;
-		}
+    
+    TINY, SMALL, MEDIUM, LARGE, HUGE, GARGANTUAN, COLOSSAL;
+    
+    /**
+     * Size in number of map squares.
+     * @return The size in map squares for this size category.
+     */
+    public int sizeSquares() {
+	switch (this) {
+	case LARGE: return 2;
+	case HUGE: return 3;
+	case GARGANTUAN: return 4;
+	case COLOSSAL: return 6;
+	default: return 1;
 	}
+    }
 	
 }
