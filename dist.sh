8aec4aa545b6432bbd18b9bef083d79eef572726
Christoph Breitkopf <chbreitkopf@gmail.com>
2005-11-30 15:37:40 +0100

    Fixed spelling error in String.

--- dist.sh
+++ dist.sh
@@ -3,5 +3,7 @@ ant jar
 version=`java -jar dist/MapExplorer.jar -version | awk '{print $4}'`
 mkdir tmp/MapExplorer
 cp dist/MapExplorer.jar lib/*.map lib/README.txt lib/MapExplorer.bat lib/mapexplorer tmp/MapExplorer
-cd tmp ; zip -9 -r MapExplorer-$version.zip MapExplorer
-cp www/* MapExplorer-$version.zip /home/breitko/prj/ddm/mapexplorer
+cd tmp
+unix2dos MapExplorer/*.bat
+zip -9 -r MapExplorer-$version.zip MapExplorer
+cp ../www/* MapExplorer-$version.zip /home/breitko/prj/ddm/mapexplorer
