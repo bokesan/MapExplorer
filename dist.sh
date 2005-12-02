c7b6f400f5f143902f252e36b8cfb55452e142bc
Christoph Breitkopf <chbreitkopf@gmail.com>
2005-12-02 16:26:47 +0100

    Added safety check.

--- dist.sh
+++ dist.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+echo "Have you bumped the version number?"
+echo "And committed?"
+echo "And done cvs tag ?"
+read answer
+if [ ! $answer = yes ]; then
+    exit 0
+fi
+
 ant jar
 version=`java -jar dist/MapExplorer.jar -version | awk '{print $4}'`
 mkdir tmp/MapExplorer
