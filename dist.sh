2e0f768dabf8e207a9f1b0904c5de9d6d9c695bb
Christoph Breitkopf <chbreitkopf@gmail.com>
2005-12-24 13:25:38 +0100

    Create source archive.

--- dist.sh
+++ dist.sh
@@ -10,8 +10,14 @@ fi
 ant jar
 version=`java -jar dist/MapExplorer.jar -version | awk '{print $4}'`
 mkdir tmp/MapExplorer
-cp dist/MapExplorer.jar lib/*.map lib/README.txt lib/MapExplorer.bat lib/mapexplorer tmp/MapExplorer
+cp dist/MapExplorer.jar lib/*.map README.txt License.txt lib/MapExplorer.bat lib/mapexplorer tmp/MapExplorer
 cd tmp
 unix2dos MapExplorer/*.bat
 zip -9 -r MapExplorer-$version.zip MapExplorer
-cp ../www/* MapExplorer-$version.zip /home/breitko/prj/ddm/mapexplorer
+
+# source
+cd ..
+zip -9 tmp/MapExplorer-$version.src.zip `find . -name '*.java' -print` build.xml License.txt \
+ lib/*.map README.txt lib/mapexplorer lib/MapExplorer.bat
+
+cp www/* tmp/MapExplorer-$version.*zip ~/prj/ddm/mapexplorer
