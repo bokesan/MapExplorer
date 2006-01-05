038e2841ef9db96001394f396d1245c417fcd882
Christoph Breitkopf <chbreitkopf@gmail.com>
2006-01-05 14:32:07 +0100

    Add properties file.

--- dist.sh
+++ dist.sh
@@ -10,7 +10,7 @@ fi
 ant jar
 version=`java -jar dist/MapExplorer.jar -version | awk '{print $4}'`
 mkdir tmp/MapExplorer
-cp dist/MapExplorer.jar lib/*.map README.txt License.txt lib/MapExplorer.bat lib/mapexplorer tmp/MapExplorer
+cp dist/MapExplorer.jar lib/*.map lib/*.properties README.txt License.txt lib/MapExplorer.bat lib/mapexplorer tmp/MapExplorer
 cd tmp
 unix2dos MapExplorer/*.bat
 zip -9 -r MapExplorer-$version.zip MapExplorer
@@ -18,6 +18,6 @@ zip -9 -r MapExplorer-$version.zip MapExplorer
 # source
 cd ..
 zip -9 tmp/MapExplorer-$version.src.zip `find . -name '*.java' -print` build.xml License.txt \
- lib/*.map README.txt lib/mapexplorer lib/MapExplorer.bat
+ lib/*.map README.txt lib/*.properties lib/mapexplorer lib/MapExplorer.bat
 
 cp www/* tmp/MapExplorer-$version.*zip ~/prj/ddm/mapexplorer
