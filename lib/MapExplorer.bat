7a88290cce23360dbda7db0b0c14fbdec05332ba
Christoph Breitkopf <chbreitkopf@gmail.com>
2006-03-14 15:46:32 +0100

    Bugfix: batch file did not work on Windows 9x, ME.

--- lib/MapExplorer.bat
+++ lib/MapExplorer.bat
@@ -1,5 +1,14 @@
 @echo off
 setlocal
+set JVM=java
 set ME_HOME=%~dp0
-java -Dmapexplorer.home=%ME_HOME% -jar %ME_HOME%\MapExplorer.jar %*
+if exist "%ME_HOME%" goto HOME_OK
+set ME_HOME=%CD%
+if exist "%ME_HOME%" goto HOME_OK
+rem ME_HOME not correctly set - try jar file w/o path
+%JVM% -jar MapExplorer.jar %*
+goto DONE
+:HOME_OK
+%JVM% -Dmapexplorer.home=%ME_HOME% -jar %ME_HOME%\MapExplorer.jar %*
+:DONE
 endlocal
