83539ce1d65b334d559d352b05f9d12ce2cf2807
Christoph Breitkopf <chbreitkopf@gmail.com>
2006-03-16 14:49:35 +0100

    Version 20060316.

--- lib/MapExplorer.bat
+++ lib/MapExplorer.bat
@@ -9,6 +9,8 @@ rem ME_HOME not correctly set - try jar file w/o path
 %JVM% -jar MapExplorer.jar %*
 goto DONE
 :HOME_OK
+rem get rid of trailing backslash or -D won't work
+if %ME_HOME:~-1%==\ set ME_HOME=%ME_HOME:~0,-1%
 %JVM% "-Dmapexplorer.home=%ME_HOME%" -jar "%ME_HOME%\MapExplorer.jar" %*
 :DONE
 endlocal
--- src/de/bokeh/ddm/mapexplorer/MapExplorer.java
+++ src/de/bokeh/ddm/mapexplorer/MapExplorer.java
@@ -1,5 +1,5 @@
 /*
- * $Id: MapExplorer.java,v 1.22 2006/03/15 14:44:43 breitko Exp $
+ * $Id: MapExplorer.java,v 1.23 2006/03/16 13:49:35 breitko Exp $
  * 
  * This file is part of Map Explorer.
  * 
@@ -41,7 +41,7 @@ import java.net.URL;
  */
 public class MapExplorer implements ActionListener, ItemListener {
 
-    public static final String VERSION = "20060315";
+    public static final String VERSION = "20060316";
     
     // ActionCommands
     private static final String ACTION_LOAD_MAP = "loadMap";
