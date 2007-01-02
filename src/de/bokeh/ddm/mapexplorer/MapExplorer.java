ee106c7cf64640cca90d88d8cf2de55e836c3c04
Christoph Breitkopf <chbreitkopf@gmail.com>
2007-01-02 12:06:04 +0100

    Version 20070102

--- src/de/bokeh/ddm/mapexplorer/MapExplorer.java
+++ src/de/bokeh/ddm/mapexplorer/MapExplorer.java
@@ -42,7 +42,7 @@ import java.net.URL;
  */
 public class MapExplorer implements ActionListener, ItemListener {
 
-    public static final String VERSION = "20061205-1";
+    public static final String VERSION = "20070102";
 
     // ActionCommands
     private static final String ACTION_LOAD_MAP = "loadMap";
